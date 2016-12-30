ed9a0a24ba2ed347ae65a9650fd16e870b16d60f
Ioannis Giagkiozis <i.giagkiozis@sheffield.ac.uk>
2016-12-30 22:20:35 +0000

    Properties update

--- PCGSharp/Properties/AssemblyInfo.cs
+++ PCGSharp/Properties/AssemblyInfo.cs
@@ -6,19 +6,16 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("PCGSharp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Permuted Congruential Generator for C# and .NET 2.0 and above.")]
 [assembly: AssemblyCompany("Bismur Studios Ltd.")]
-[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Bismur Studios Ltd. (c) 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("1.0.*")]
+[assembly: InternalsVisibleTo("PCGSharp.Tests")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
