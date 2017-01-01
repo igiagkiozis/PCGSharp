70cd942b829100150fb5ed0c2165a4f0544582e4
Ioannis Giagkiozis <i.giagkiozis@bismur.co.uk>
2017-01-01 12:50:14 +0000

    case issue fix

---
+++ PCGSharp/Source/PcgSeed.cs
@@ -0,0 +1,47 @@
+﻿// MIT License
+// 
+// Copyright (c) 2016 Bismur Studios Ltd.
+// Copyright (c) 2016 Ioannis Giagkiozis
+//
+// This file is part of PCGSharp.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial 
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+//  LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN 
+//  NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+
+
+namespace PCGSharp {
+
+  public static class PcgSeed {
+
+    /// <summary>
+    /// Provides a time-dependent seed value, matching the default behavior of System.Random.
+    /// </summary>
+    public static ulong TimeBasedSeed() {      
+      return (ulong)(Environment.TickCount);
+    }
+
+    /// <summary>
+    /// Provides a seed based on time and unique GUIDs.
+    /// </summary>
+    public static ulong GuidBasedSeed() {
+      ulong upper = (ulong)(Environment.TickCount ^ Guid.NewGuid().GetHashCode()) << 32;
+      ulong lower = (ulong)(Environment.TickCount ^ Guid.NewGuid().GetHashCode());
+      return (upper | lower);
+    }
+
+  }
+}
